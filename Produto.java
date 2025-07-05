62c60ea97d6433e922b19c16a0322d7c244a03ad
João <105893644+JotaVSRodrigues@users.noreply.github.com>
2025-07-05 14:09:43 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.a08;
+
+public class Produto {
+    /*
+    HERANÇA
+    
+    classe Produto (atual) -> classe pai ou superclasse
+    classe ProdutoAlimenticio -> classe filha ou classe derivada
+    classe ProdutoEletronico -> classe filha ou classe derivada
+    
+    extends -> palavra chave que permite de uma classe derive informações de outra
+    */
+    
+    
+}
